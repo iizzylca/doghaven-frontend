41944de686d0107b47a14a09bb7f16e013686ed6
github username <github email address>
2020-12-04 05:47:43 -0500

    Display in progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('hello')
+
+const url = 'http://localhost:3000/api/v1/rescues/1'
+const header = document.querySelector('.header')
+
+const getRescue = () => {
+    fetch(url)
+    .then(r => r.json())
+    .then( data => {
+        console.log(data)
+    })
+}
+
+const renderRescue = () => {
+    
+}
+
+getRescue();
